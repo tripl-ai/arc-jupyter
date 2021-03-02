c12a6b2c0c965f647dd37eef32e32d01f694ade2
Mike Seddon <seddonm1@gmail.com>
2021-03-03 10:58:28 +1100

    3.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object Dependencies {
   // Versions
-  lazy val sparkVersion = "3.0.1"
+  lazy val sparkVersion = "3.1.1"
 
   val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion % "provided"
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
   val sparkHive = "org.apache.spark" %% "spark-hive" % sparkVersion % "provided"
   val sparkMl = "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided"
 
-  val kernel = "sh.almond" %% "kernel" % "0.10.9"
+  val kernel = "sh.almond" %% "kernel" % "0.11.0"
 
-  val arc = "ai.tripl" %% "arc" % "3.7.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.8.0" % "provided"
 
   val etlDeps = Seq(
     sparkCore,
